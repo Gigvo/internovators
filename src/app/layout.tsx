669745f71fe1ff3f9bb3193c62040925ff7fd64a
Gigvo <giganiusrevo@gmail.com>
2025-04-27 16:57:12 +0700

    fix the calendar.tsx in components/ui issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 // import Navbar from "@/components/navbar";
+import "react-day-picker/style.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
